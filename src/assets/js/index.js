c22b9fbe8f04cbfb4ec35762c0d882aaeae89598
AD <atar.dci@gmail.com>
2019-09-16 21:31:54 +0200

    Set demo duration

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,79 +1,84 @@
-import "../scss/styles.scss"
-import "../img/background.jpg"
+import '../scss/styles.scss';
+import '../img/background.jpg';
 
 var moment = require('moment');
 
 class timer {
-    constructor(domselector, date) {
-        this.container = this.ele(domselector)
-        this.endDate = moment(date, "DD.MM.YYYY hh:mm")
-        this.currentTime = moment()
-        this.duration = this.endDate - this.currentTime
-        this.interval = 1000
+  constructor(domselector, date) {
+    this.container = this.ele(domselector);
+    this.endDate = moment(date, 'DD.MM.YYYY hh:mm');
+    this.currentTime = moment();
 
-        this.counter = {
-            days: 0,
-            hours: 0,
-            minutes: 0,
-            seconds: 0
-        }
+    // 2 Weeks for demo
+    this.duration = 1209600000;
 
-        this.update()
-    }
+    // Time to specified date when available
+    // this.duration = this.endDate - this.currentTime;
+    this.interval = 1000;
 
-    ele(element) {
-        return document.querySelector(element)
-    }
+    this.counter = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
 
-    update() {
-        let clock = setInterval(
-            () => {
-                if (this.duration > this.interval) {
-                    this.count();
-                } else {
-                    clearInterval(clock)
-                    this.stop()
-                    this.magic()
-                }
+    this.update();
+  }
 
-                // Happens in every round
-                this.printOutput();
-            }, this.interval)
-    }
+  ele(element) {
+    return document.querySelector(element);
+  }
 
-    count() {
-        this.duration = moment.duration(
-            this.duration - this.interval,
-            "milliseconds"
-        )
-        this.counter.days = Math.floor(this.duration._milliseconds / 24 / 3600 / 1000)
-        this.counter.hours = this.duration.hours()
-        this.counter.minutes = this.duration.minutes()
-        this.counter.seconds = this.duration.seconds()
-    }
+  update() {
+    let clock = setInterval(() => {
+      if (this.duration > this.interval) {
+        this.count();
+      } else {
+        clearInterval(clock);
+        this.stop();
+        this.magic();
+      }
 
-    stop() {
-        this.counter.days = 0
-        this.counter.hours = 0
-        this.counter.minutes = 0
-        this.counter.seconds = 0
+      // Happens in every round
+      this.printOutput();
+    }, this.interval);
+  }
 
-    }
+  count() {
+    this.duration = moment.duration(
+      this.duration - this.interval,
+      'milliseconds'
+    );
+    this.counter.days = Math.floor(
+      this.duration._milliseconds / 24 / 3600 / 1000
+    );
+    this.counter.hours = this.duration.hours();
+    this.counter.minutes = this.duration.minutes();
+    this.counter.seconds = this.duration.seconds();
+  }
 
-    magic() {
-        document.querySelector('h1').innerHTML = "It's time!"
-        setTimeout(() => {
-            document.querySelector('h1').innerHTML = "Are you excited?!"
-        }, 3000);
-        setTimeout(() => {
-            document.querySelector('#overlay').style.display = 'block'
-            document.querySelector('.wrap').innerHTML = ""
-        }, 6000);
-    }
+  stop() {
+    this.counter.days = 0;
+    this.counter.hours = 0;
+    this.counter.minutes = 0;
+    this.counter.seconds = 0;
+  }
 
-    template() {
-        const { days, hours, minutes, seconds } = this.counter
-        return `
+  magic() {
+    document.querySelector('h1').innerHTML = "It's time!";
+    setTimeout(() => {
+      document.querySelector('h1').innerHTML = 'Are you excited?!';
+    }, 3000);
+    setTimeout(() => {
+      document.querySelector('#overlay').style.display = 'block';
+      document.querySelector('.wrap').innerHTML = '';
+    }, 6000);
+  }
+
+  template() {
+    const { days, hours, minutes, seconds } = this.counter;
+    return `
         <div class="timeOutput">
         <span>Days</span>
         <div id="days">${days}</div>
@@ -90,12 +95,13 @@ class timer {
         <span>Seconds</span>
         <div id="seconds">${seconds}</div>
     </div>
-        `
-    }
+        `;
+  }
 
-    printOutput() {
-        this.container.innerHTML = this.template()
-    }
+  printOutput() {
+    this.container.innerHTML = this.template();
+  }
 }
 
-const countDown = new timer('#output', '01.05.2019 00:00')
+// Specifies end date in DD.MM.YYYY hh:mm format
+const countDown = new timer('#output', '01.08.2019 00:00');
